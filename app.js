55071064c0f73afc1e1a46c8024dddaed21d5562
eliezerwohl <eliezerwohl@gmail.com>
2016-02-11 15:48:33 -0500

    installed all teh packages needed to do the homework

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var PORT = 8080 || process.env.NODE_ENV;
+var mysql = require('mysql');
 
 app.get("/", function(req, res){
   res.send("hey mr plinkett");
