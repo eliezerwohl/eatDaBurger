d232d852ef652b293590a3c39c515cfb326e3a43
eliezerwohl <eliezerwohl@gmail.com>
2016-02-13 17:14:09 -0500

    made function make a burger in msql database, and change devoured to 1

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,18 @@
 var connection =require("./connection.js");
+
+// creates a new burger 
+connection.query('INSERT INTO burgers (burgerName) values (?)',[userInput], function(err, data) {
+  if (err) throw err;
+});
+
+// changes burger to devoured 
+
+connection.query('update burgers set devoured = 1 where burgerName = ?',[userEat], function(err, data) {
+  if (err) throw err;
+});
+
 connection.query('SELECT * FROM burgers', function(err, col) {
   if (err) throw err;
  
-  console.log('The solution is: ', col);
+  console.log('this is information: ', col);
 });
