1d4948640b39f2ba426556f04735e3be31a4d637
eliezerwohl <eliezerwohl@gmail.com>
2016-02-16 14:50:41 -0500

    fixed spacing issues

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,29 +2,14 @@ var burger = require ("../models/burger.js");
 var express = require("express");
 var app = express();
 var router = express.Router();
-
-
-
-
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 var PORT = process.env.NODE_ENV || 8080;
-// var methodOverride = require('Method-override');
-
-
-// app.get("/", function(req, res){
-//   burger.allBurger(function(data){
-//     res.render('index', {data});
-
-//     })
-//   });
-
 router.get('/', function(req,res) {
   console.log('yup')
  burger.allBurger(function(data){
     res.render('index', {data});
-
   });
 });
 
@@ -32,31 +17,10 @@ router.post("/newburger", function(req, res) {
   burger.newBurger(req.body.burgerName);
   res.redirect("/")
 })
-
 router.post("/devour/:burgerName", function(req, res) {
   console.log(req.params.burgerName)
   burger.devour(req.params.burgerName)
   res.redirect("/")
 })
-
-// app.get("/viewAll", function(req, res){
-// burger.allBurger();
-//   res.redirect("/")
-// })
-
-// app.get("/newBurger/:burgerName", function(req, res){
-//   burger.newBurger(req.params.burgerName);
-//   res.redirect("/")
-// })
-
-// app.get("/devour/:burgerName", function(req, res){
-//   burger.devour(req.params.burgerName);
-//   res.redirect("/")
-// })
-
-// app.listen(PORT,function(){
-
-//   console.log("Listenign on %s", PORT)
-// })
 
 module.exports = router;
