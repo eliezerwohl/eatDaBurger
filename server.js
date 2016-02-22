68404574fc0ac572388ed9bc12909869a8427670
eliezerwohl <eliezerwohl@gmail.com>
2016-02-22 14:29:29 -0500

    trying to fix heroku problem

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT  || 8080;
+var PORT = process.env.NODE_ENV || 8000;
 var methodOverride = require('Method-override');
 var bodyParser = require('body-parser');
 var expressHandlebars = require('express-handlebars');
