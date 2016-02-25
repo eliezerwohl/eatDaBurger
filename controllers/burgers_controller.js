081989389516bc94474a16b1bd332ffc3ef0dbd5
eliezerwohl <eliezerwohl@gmail.com>
2016-02-24 22:40:41 -0500

    sequelize

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,17 +1,25 @@
 var burger = require ("../models/burger.js");
 var express = require("express");
+var mysql = require('mysql');
 var app = express();
 var router = express.Router();
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 var PORT = process.env.NODE_ENV || 8080;
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('burgers_db', 'root');
+
 router.get('/', function(req,res) {
-  console.log('yup')
- burger.allBurger(function(data){
-    res.render('index', {data});
+var s = 'SELECT * FROM burgers;';
+sequelize.query(s).spread(function(results, metadata) {
+  console.log(results)
+  // Results will be an empty array and metadata will contain the number of affected rows.
+  res.render('index', {results});
+})
   });
-});
+
 
 router.post("/newburger", function(req, res) {
   burger.newBurger(req.body.burgerName);
