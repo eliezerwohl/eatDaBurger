0558eead15e28252c69374c897e62e467514767e
eliezerwohl <eliezerwohl@gmail.com>
2016-03-05 16:31:32 -0500

    hk

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var Sequelize = require('sequelize');
 
 if(process.env.NODE_ENV === 'production') {
   // HEROKU DB
-  console.log(process.env.JAWSDB_URL);
+  // console.log(process.env.JAWSDB_URL);
 
   var connection = new Sequelize(process.env.JAWSDB_URL);
 }
